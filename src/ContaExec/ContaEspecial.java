17d15c1ef6002467961c7864e772c05ef60f269f
HenriqFonseca <henrique12095@gmail.com>
2023-09-15 20:34:17 -0300

    Lógica método sacar funcionando

--- src/ContaExec/ContaEspecial.java
+++ src/ContaExec/ContaEspecial.java
@@ -1,23 +1,42 @@
 package ContaExec;
 
+
+import java.io.IOException;
+
 public class ContaEspecial extends ContaCorrente{
 
     private float limite;
 
-    public ContaEspecial(float saldo, float limte){
+    public ContaEspecial(float saldo, float limite){
         super(saldo);
+        this.limite = limite;
+    }
+
+    public float getLimite() {
+        return limite;
+    }
+
+    public void setLimite(float limite) {
         this.limite = limite;
     }
 
     @Override
     public float sacar(float valorSaque){
-        if(valorSaque>this.saldo){
+        if(valorSaque>(this.limite+this.saldo)){
+            return 01;
+        }
+        else if(valorSaque>this.saldo){
             float diferenca = valorSaque - this.saldo;
-            this.saldo -= diferenca;
-            float menosLimite = valorSaque - diferenca;
-            return this.limite - menosLimite;
+            float novoSaldo = (this.saldo - valorSaque) + diferenca;
+            this.setSaldo(novoSaldo);
+
+            float novoLimite = this.limite - diferenca;
+            this.setLimite(novoLimite);
+            return novoSaldo;
         }else{
-            return this.saldo - valorSaque;
+            float novoSaldo = this.saldo - valorSaque;
+            this.setSaldo(novoSaldo);
+            return novoSaldo;
         }
     }
 }
--- src/ContaExec/Main.java
+++ src/ContaExec/Main.java
@@ -11,15 +11,31 @@ public class Main {
         float valorSaque = 110;
         float diferenca;
 
-        if(valorSaque>saldo){
-            System.out.println(diferenca = valorSaque - saldo);
-            System.out.println("\nSALDOOO\n " + ((saldo - valorSaque) + diferenca));
-            float menosLimite = limite - diferenca;
-            System.out.println("Limite: "+ menosLimite +
-                    "\nSaldo atual: " + saldo);
-        }else{
-            System.out.println(saldo - valorSaque);
+        ContaEspecial contEspec = new ContaEspecial(100,200);
+
+
+        float teste = contEspec.sacar(300);
+
+        if(teste ==01){
+            System.out.println("\nSaldo e limite Insuficientes" +
+                    "\n =====================================");
         }
+
+        System.out.println(contEspec.getLimite());
+
+        System.out.println("\nLimite atual: "+ contEspec.getLimite()+
+                "Saldo atual: "+ contEspec.getSaldo());
+
+//        if(valorSaque>saldo){
+//            System.out.println(diferenca = valorSaque - saldo);
+//            saldo = ((saldo - valorSaque) + diferenca);
+//            System.out.println("\nSALDOOO " + saldo);
+//            float menosLimite = limite - diferenca;
+//            System.out.println("Limite: "+ menosLimite +
+//                    "\nSaldo atual: " + saldo);
+//        }else{
+//            System.out.println(saldo - valorSaque);
+//        }
 
     }
 }
